e990a2dec7d69e0d18eead00820d9c74b0358d5c
wotjd243 <wotjd243@naver.com>
2019-04-25 19:16:57 +0900

    D01.04-Exercise-PokemonNumberValueObject

--- src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
+++ src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
@@ -1,6 +1,8 @@
 package io.github.wotjd243.dddpokemon.pokemon.domain;
 
 public class Pokemon {
+    // TODO (1) 원시 데이터 형식 int를 NationalPokedexNumber로 리팩터링
+    // TODO (2) 1에서 151 사이의 값을 갖도록 유효성 검사
     private final int number;
     private final String name;
     private final int captureRate;
