5ea76420a075bfc4df0a097d958687d3d4efa237
wotjd243 <wotjd243@naver.com>
2019-04-25 17:43:47 +0900

    D01.01-Exercise-Glossary

---
+++ src/main/java/io/github/wotjd243/dddpokemon/DddPokemonApplication.java
@@ -0,0 +1,13 @@
+package io.github.wotjd243.dddpokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPokemonApplication.class, args);
+    }
+
+}
