ea984b4c939ed69b09800921db172bff0ee916d1
wotjd243 <wotjd243@naver.com>
2019-05-02 15:42:49 +0900

    D02.01-Exercise-SpringBootApplication

--- src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
+++ src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
@@ -1,8 +1,6 @@
 package io.github.wotjd243.dddpokemon.pokemon.domain;
 
 public class Pokemon {
-    // COMPLETED (1) 원시 데이터 형식 int를 NationalPokedexNumber로 리팩터링
-    // COMPLETED (2) 1에서 151 사이의 값을 갖도록 유효성 검사
     private NationalPokedexNumber number;
     private final String name;
     private final int captureRate;
---
+++ src/main/java/io/github/wotjd243/dddpokemon/utils/NetworkUtils.java
@@ -0,0 +1,27 @@
+package io.github.wotjd243.dddpokemon.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public final class NetworkUtils {
+    private final static Logger logger = LoggerFactory.getLogger(NetworkUtils.class);
+
+    private NetworkUtils() {
+    }
+
+    public static URL parseUrl(final String spec) {
+        URL url = null;
+        try {
+            url = new URL(spec);
+        } catch (final MalformedURLException e) {
+            logger.error("Parsing failed", e);
+        }
+
+        logger.debug("Parsed URL {}", url);
+
+        return url;
+    }
+}
