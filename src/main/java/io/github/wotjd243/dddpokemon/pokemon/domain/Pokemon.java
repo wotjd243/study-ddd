e2553ee3f89daa72044c373760a450b14d10fae2
wotjd243 <wotjd243@naver.com>
2019-05-02 15:31:47 +0900

    D01.04-Solution-PokemonNumberValueObject

--- src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
+++ src/main/java/io/github/wotjd243/dddpokemon/pokemon/domain/Pokemon.java
@@ -1,14 +1,14 @@
 package io.github.wotjd243.dddpokemon.pokemon.domain;
 
 public class Pokemon {
-    // TODO (1) 원시 데이터 형식 int를 NationalPokedexNumber로 리팩터링
-    // TODO (2) 1에서 151 사이의 값을 갖도록 유효성 검사
-    private final int number;
+    // COMPLETED (1) 원시 데이터 형식 int를 NationalPokedexNumber로 리팩터링
+    // COMPLETED (2) 1에서 151 사이의 값을 갖도록 유효성 검사
+    private NationalPokedexNumber number;
     private final String name;
     private final int captureRate;
 
     public Pokemon(final int number, final String name, final int captureRate) {
-        this.number = number;
+        this.number = NationalPokedexNumber.valueOf(number);
         this.name = name;
         this.captureRate = captureRate;
     }
