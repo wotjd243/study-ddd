64b5096d728eaf50689c3002eae62a5466bc593b
wotjd243 <wotjd243@naver.com>
2019-05-09 19:20:00 +0900

    D03.03-Excercise-FirstCollection

--- src/main/java/io/github/wotjd243/dddpokemon/Application.java
+++ src/main/java/io/github/wotjd243/dddpokemon/Application.java
@@ -8,10 +8,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    // COMPLETED (1) trainer > domain에 Trainer라는 ENTITY를 만든다.
-    // COMPLETED (2) Trainer는 아이디와 레벨을 가진다.
-    // COMPLETED (3) Trainer에 @Entity를 붙인다.
-    // COMPLETED (4) @Id를 이용해서 식별자를 지정한다.
-    // COMPLETED (4) trainer > domain에 TrainerRepository를 만든다.
-    // COMPLETED (5) trainer > infra에 TrainerRepository를 구현한 JpaTrainerRepository를 만든다.
+    // TODO (1) Trainer에 가지고 있는 포켓몬 리스트를 추가한다.
+    // TODO (2) Trainer AGGREGATE에 잡힌 포켓몬을 뜻하는 PokemonCaught VALUE OBJECT를 만든다.
+    // TODO (3) 가지고 있는 포켓몬 리스트는 Party라는 일급 콜렉션으로 만든다.
+    // TODO (4) Party와 PokemonCaught에 @Embeddable를 붙인다.
+    // TODO (5) Party가 가지고 있는 콜렉션에 @ElementCollection을 붙인다.
+    // TODO (6) Trainer의 party에 @Embedded를 붙인다.
 }
