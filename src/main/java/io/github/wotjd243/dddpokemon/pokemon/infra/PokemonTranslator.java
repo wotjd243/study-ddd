a0a4ad5da40782ff33f79e9b4d49a74a12eb3e37
wotjd243 <wotjd243@naver.com>
2019-05-16 16:32:41 +0900

    D04.01-Exercise-PokemonCatching

--- src/main/java/io/github/wotjd243/dddpokemon/pokemon/infra/PokemonTranslator.java
+++ src/main/java/io/github/wotjd243/dddpokemon/pokemon/infra/PokemonTranslator.java
@@ -1,19 +1,32 @@
 package io.github.wotjd243.dddpokemon.pokemon.infra;
 
 import io.github.wotjd243.dddpokemon.pokemon.domain.Pokemon;
+import me.sargunvohra.lib.pokekotlin.model.Name;
 import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
 public class PokemonTranslator {
+    private static final int LANGUAGE_KOREAN_ID = 3;
+
     public Optional<Pokemon> translate(final PokemonSpecies pokemonSpecies) {
         final Pokemon pokemon = new Pokemon(
                 pokemonSpecies.getId(),
-                pokemonSpecies.getName(),
+                getName(pokemonSpecies.getNames(), LANGUAGE_KOREAN_ID, pokemonSpecies.getName()),
                 pokemonSpecies.getCaptureRate()
         );
         return Optional.ofNullable(pokemon);
+    }
+
+    public String getName(final List<Name> names, final int languageId, final String defaultName) {
+        return names.stream()
+                .filter(name -> name.getLanguage().getId() == languageId)
+                .findAny()
+                .map(Name::getName)
+                .orElse(defaultName)
+                ;
     }
 }
